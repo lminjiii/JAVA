9cb2c137b0887ad2672a36b80145d54db4dde3c6
lminjiii <lminjiiiii@gmail.com>
2024-12-06 09:32:20 +0900

    Add files via upload

---
+++ 241205/MinjiStore.java
@@ -0,0 +1,17 @@
+package basic.method;
+
+public class MinjiStore {
+    int minusNumber(int numA, int numB) {
+        return numA - numB;
+    }
+
+    int addNumbers(int numA, int numB) {
+//        int sum = numA + numB;
+        return numA + numB;
+
+    }
+
+    void printTitle(String data) {
+        System.out.println(data);
+    }
+}
